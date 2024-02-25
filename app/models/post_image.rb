865c220fca5f71da719a06d1e5bdb6bb458ffe27
ruri <devruri@gmail.com>
2024-02-26 00:24:30 +0900

    イメージアップロードの実装

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,10 @@
 class PostImage < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   belongs_to :post
   has_one_attached :image
+
+  def image_url
+    image.attached? ? url_for(image) : nil
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_25_135541) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_25_140031) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -69,6 +69,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_25_135541) do
     t.datetime "updated_at", null: false
     t.index ["condition_id"], name: "index_post_conditions_on_condition_id"
     t.index ["post_id"], name: "index_post_conditions_on_post_id"
+  end
+
+  create_table "post_images", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.integer "order_no"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_images_on_post_id"
   end
 
   create_table "post_tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -139,6 +147,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_25_135541) do
   add_foreign_key "conditions", "condition_groups"
   add_foreign_key "post_conditions", "conditions"
   add_foreign_key "post_conditions", "posts"
+  add_foreign_key "post_images", "posts"
   add_foreign_key "post_tags", "posts"
   add_foreign_key "post_tags", "tags"
   add_foreign_key "posts", "categories"
