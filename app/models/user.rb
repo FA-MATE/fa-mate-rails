b497ccfbc324a67679eed1647c44c35b36d2d7d0
ruri <devruri@gmail.com>
2024-03-06 21:53:40 +0900

    ログインしたユーザの通知設定取得を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :user_conditions
   has_many :conditions, through: :user_conditions
+
+  def notification_settings
+    UserNotificationSetting.where(user_id: self.id).first
+  end
 end
