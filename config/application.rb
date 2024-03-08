ba721b3ea0a009d2e62548b7b35112f10d1f4421
ruri <devruri@gmail.com>
2024-03-08 19:48:40 +0900

    自動生成されるrbファイルについてRubocop

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,13 @@ module FaMateRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # 自動生成されるrbファイルについてRubocop
+    config.generators.after_generate do |files|
+      parsable_files = files.filter { |file| file.end_with?('.rb') }
+      unless parsable_files.empty?
+        system("bin/rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
+      end
+    end
   end
 end
