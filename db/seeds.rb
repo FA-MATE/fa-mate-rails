947d3b78478e29aa7c814d4ce10304eae8424561
ruri <devruri@gmail.com>
2024-03-13 18:34:06 +0900

    db/seeds 更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,103 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
 
-user = User.find_or_create_by(nickname: 'テストユーザ')
-category = Category.find_or_create_by(name: 'ペット', order_no: 1)
-sub_category = SubCategory.find_or_create_by(category: category, name: '犬', order_no: 1)
-SubCategory.find_or_create_by(category: category, name: '猫', order_no: 2)
-condition_group = ConditionGroup.find_or_create_by(name: '居住')
-condition = Condition.find_or_create_by(condition_group: condition_group, name: '条件無し', order_no: 1)
-tag_group = TagGroup.find_or_create_by(name: '地域', order_no: 1)
-tag = Tag.find_or_create_by(tag_group: tag_group, name: '東京都', order_no: 1)
-post = Post.find_or_create_by(category: category,
-sub_category: sub_category,
-user: user,
-title: 'テストタイトル',
-body: <<~TEXT)
-テストボディー
-マルチラインのテキストが
-どのような形で
-表現されるか
-確認したい
-TEXT
-post_tag = PostTag.find_or_create_by(post: post, tag: tag)
-user_condition = UserCondition.find_or_create_by(user: user, condition: condition)
-post_condition = PostCondition.find_or_create_by(post: post, condition: condition)
+## データクレーンジング
+PostTag.delete_all
+PostImage.destroy_all
+PostCondition.delete_all
+Post.delete_all
+Tag.delete_all
+TagGroup.delete_all
+SubCategory.delete_all
+Category.delete_all
+UserCondition.delete_all
+User.delete_all
+Condition.delete_all
+ConditionGroup.delete_all
+
+condition_groups = {
+  '年齢': %w[条件無し 20歳以上 大人を含むの家族構成],
+  '居住形態': %w[条件無し マンション可 一戸建て以上 庭付き一戸建て以上],
+  '今後のケア': %w[条件無し 連絡可能 毎月の近況共有可能]
+}.map.with_index(1) do |kv, order_no|
+  name, conditions = kv
+  ConditionGroup.create(name:, order_no:).tap do |condition_group|
+    conditions.each.with_index(1) do |condition_name, condition_order_no|
+      Condition.create(condition_group_id: condition_group.id, name: condition_name, order_no: condition_order_no)
+    end
+  end
+end
+
+users = %w[user hoge fuga].map.with_index do |nickname, id|
+  user = User.new(id:, nickname:)
+  condition_groups.each do |condition_group|
+    user.conditions << condition_group.conditions[rand(condition_group.conditions.length)]
+  end
+  user.save!
+  user
+end
+
+categories = {
+  '犬': %w[品種不明な犬 トイプードル チワワ ロングコートチワワ 柴犬 タイニープードル ミニチュアダックスフンド フレンチブルドッグ ポメラニアン 豆柴 ティーカッププードル],
+  '猫': %w[品種不明な猫 ミックス スコティッシュフォールド マンチカン ミヌエット ラグドール ノルウェージャンフォレストキャット サイベリアン メインクーン ブリティッシュショートヘア ベンガル アメリカンショートヘア],
+  '鳥': %w[品種不明な鳥 タイハクオウム ワカケホンセイインコ ヨウム キバタン ズクロオトメインコ クロクモインコ モモイロインコ セキセイインコ オカメインコ ボタンインコ コガネメキシコインコ ナナクサインコ
+           バライロビタイウロコインコ ベニコンゴウインコ ルリコンゴウインコ ナナイロメキシコインコ ワキコガネウロコインコ ホオミドリアカウロコインコ サザナミインコ キソデインコ オオホンセイインコ ショウジョウインコ シロハラインコ 九官鳥]
+}.map.with_index(1) do |kv, order_no|
+  name, sub_categories = kv
+  Category.create(name:, order_no:).tap do |category|
+    sub_categories.each.with_index(1) do |sub_category_name, sub_category_name_order_no|
+      SubCategory.create(category_id: category.id, name: sub_category_name, order_no: sub_category_name_order_no)
+    end
+  end
+end
+
+tag_groups = {
+  '地域': %w[関東 中部 中国 四国 中部 九州 沖縄 近畿 北海道 東北],
+  '年齢': %w[1ヶ月以内 3ヶ月以内 6ヶ月以内 9ヶ月以内 1年以内 3年以内 6年以内 9年以内 10年以上],
+  '性別': %w[不明 オス メス],
+  'ワクチン接種': %w[未接種 接種済]
+}.map.with_index(1) do |kv, order_no|
+  name, tags = kv
+  TagGroup.create(name:, order_no:).tap do |tag_group|
+    tags.each.with_index(1) do |tag_name, tag_order_no|
+      Tag.create(tag_group_id: tag_group.id, name: tag_name, order_no: tag_order_no)
+    end
+  end
+end
+
+tag_groups ||= TagGroup.all.to_a
+condition_groups ||= ConditionGroup.all.to_a
+categories ||= Category.all.to_a
+users ||= User.all.to_a
+30.times do |i|
+  category = categories[rand(categories.size)]
+  post = Post.new(
+    category:,
+    sub_category: category.sub_categories[rand(category.sub_categories.size)],
+    user_id: users[rand(users.size)].id,
+    title: "#{i + 1}: 愛護センターより保護　豆柴風MIX",
+    body: <<~BODY
+小柄でお散歩大好き、甘えん坊のシニア犬です
+引っ張らず、無駄吠えせず、上手に歩きます
+畑仕事やお散歩のパートナーにピッタリです
+引っ張らないので、ご高齢の方でも大丈夫です
+
+まだ苦手な事が少しありますが　こちらが気をつけてあげれば大丈夫な程度です。信頼関係が深まれば苦手な事も大丈夫になる可能性があります
+
+良く寝て、良く食べます
+BODY
+  )
+  tag_groups.each do |tag_group|
+    post.tags << tag_group.tags[rand(tag_group.tags.size)]
+  end
+  condition_groups.each do |condition_group|
+    post.conditions << condition_group.conditions[rand(condition_group.conditions.size)]
+  end
+
+  3.times do
+    file_name = "pet#{rand(11)}.jpg"
+    post_image = PostImage.new
+    post_image.image.attach(io: File.open("public/images/dummy/#{file_name}"), filename: file_name)
+    post.post_images << post_image
+  end
+  post.save!
+end
